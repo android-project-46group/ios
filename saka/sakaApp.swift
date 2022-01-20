972ac42b7a8e8bf7f2604fd8a9b6ae26d737ad8b
takahiro.tominaga <tt.300607@gmail.com>
2022-01-21 07:15:08 +0900

    first commit

---
+++ saka/sakaApp.swift
@@ -0,0 +1,17 @@
+//
+//  sakaApp.swift
+//  saka
+//
+//  Created by Takahiro Tominaga on 2022/01/21.
+//
+
+import SwiftUI
+
+@main
+struct sakaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
